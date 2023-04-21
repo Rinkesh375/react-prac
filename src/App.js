e21203f26179786d8eb21e9fb80bd1167c619c6c
Rinkesh375 <rinkeshujjwal16@gmail.com>
2023-04-21 11:59:17 +0530

    day-2 preclass-2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Button ({text,onClick}){
+  return <button onClick={onClick}>{text}</button>
+}
+function Header({text}){
+    return <h1>{text}</h1>
+}
 function App() {
-  function add(){
-    alert(`Add has been clicked`)
-  }
-  function reduce(){
-    alert(`Reduce has been clicked`)
-  }
+
   return (
     <div className="App">
-     <h1>Counter 0</h1>
-     <button onClick={add}>ADD</button>
-     <button onClick={reduce}>REDUCE</button>
+    <Header text="COUNTER - 0"/>
+    <Button text="ADD" onClick={()=>alert(`ADD has been clicked`)}/>
+    <Button text="REDUCE" onClick={()=>alert(`REDUCE has been clicked`)} />
     </div>
   );
 }
